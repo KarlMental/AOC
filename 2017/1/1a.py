ba5c774467985af840be8eb7cb0874b16f0cb883
Karl Blom <karlan@ongame.com>
2017-12-01 10:12:02 +0100

    First Day Cleanup

--- 2017/1/1a.py
+++ 2017/1/1a.py
@@ -1,40 +1,19 @@
-# coding: utf-8
-
-# In[42]:
-
-
 with open('1a.input', 'r') as f:
     data = f.read().split('\n')
 
-
-# In[43]:
-
-
 new_data = [int(d) for d in data[0]]
-
-
-# In[50]:
-
 
 sumz=0
 for i in range(0, len(new_data)):
-    if i &lt; len(new_data)-1 and new_data[i] == new_data[i+1]:
+    if i < len(new_data)-1 and new_data[i] == new_data[i+1]:
         sumz+=new_data[i]
     elif i == len(new_data)-1 and new_data[i] == new_data[0]:
         sumz+=new_data[i]
 print(sumz)
 
-
-# In[44]:
-
-
 length = int(len(new_data)/2)
 a = new_data[length:]
 b = new_data[:length]
-
-
-# In[45]:
-
 
 sumz=0
 for i in range(0, len(a)-1):
