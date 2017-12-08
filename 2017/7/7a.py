b5b60ae885443e8c996c9039222a0dd605257251
Karl Blom <karlan@ongame.com>
2017-12-08 15:13:25 +0100

    Day 7 - read at own peril

--- 2017/7/7a.py
+++ 2017/7/7a.py
@@ -1,4 +1,5 @@
-import copy
+from collections import Counter
+
 with open('7a.input', 'r') as f:
     inp = f.read().split('\n')
 
@@ -26,19 +27,56 @@ while True:
 		break
 	i += 1
 
+
+
 for line in inp:
 	programs[line[0]] = {}
+	programs[line[0]]['references'] = []
 	if len(line) > 2:
-		programs[line[0]]['references'] = line[3:]
+		for ref in line[3:]:
+			programs[line[0]]['references'].append([ref, None])
 	programs[line[0]]['number'] = int(line[1][1:-1])
 
 for line in inp:
-	programs[line[0]]['reference_sum'] = 0
+	for i in programs[line[0]]['references']:
+		i[1] = programs[i[0]]['number']
+
+def set_level(item, level, programs):
+	programs[item]['level'] = level
 	try:
-		for ref in programs[line[0]]['references']:
-			programs[line[0]]['reference_sum'] += programs[ref]['number']
+		for ref in programs[item]['references']:
+			set_level(ref[0], level+1, programs)
 	except:
 		pass
-reference_sums = [programs[line[0]]['reference_sum'] + programs[line[0]]['number'] for line in inp]
-print(reference_sums)
-#print(reference_sums.sort())
+
+ref_numbers = [[item[1] for item in programs[line[0]]['references'][:]] for line in inp if programs[line[0]]['references']]
+
+reference_sums = sorted([sum(item) for item in ref_numbers])
+
+for item in programs.items():
+	if item[1]['references']:
+		if sum(x[1] for x in item[1]['references']) == reference_sums[-1]:
+			set_level(item[0], 0, programs)
+
+max_level = max([value['level'] for value in programs.values()])
+
+for i in range(max_level, -1, -1):
+	for item in programs.items():
+		if i == max_level:
+			programs[item[0]]['refsum'] = programs[item[0]]['number']
+		elif item[1]['level'] == i:
+			programs[item[0]]['refsum'] = sum([programs[ref[0]]['refsum'] for ref in programs[item[0]]['references']]) + programs[item[0]]['number']
+
+for line in inp:
+	for i in programs[line[0]]['references']:
+		i[1] = programs[i[0]]['refsum']
+
+for i in range(max_level-1, -1, -1):
+	for item in programs.items():
+		if item[1]['level'] == i and item[1]['references']:
+			if item[1]['references'][0][1] != sum(x[1] for x in item[1]['references'])/sum([1 for x in item[1]['references']]):
+				print(item[1]['references'])
+
+
+#FUUUCK IIIIT
+print(programs['cwwwj'])
