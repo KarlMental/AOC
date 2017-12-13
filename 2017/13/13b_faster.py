e889e1036d8d2ef35e90f107d7cfe5f9f3dea111
Karl Blom <karlan@ongame.com>
2017-12-13 11:56:23 +0100

    13b faster

--- 2017/13/13b_faster.py
+++ 2017/13/13b_faster.py
@@ -3,14 +3,11 @@ with open('i', 'r') as f:
 layers = [(int(i.split(': ')[0]), int(i.split(': ')[1])) for i in inp]
 
 delay = 0
-brk = False
 while True:
 	for layer in layers:
 		if (layer[0]+delay) % (layer[1]*2-2) == 0:
 			break
 	else:
-		brk = True 
-	if brk:
 		break
 	delay += 1
 	
