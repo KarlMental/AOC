7c8e8ff3e9a1b40076bf6cc20c2c23443c29bb56
KarlMental <karl.e.andersson@gmail.com>
2017-12-18 07:19:07 +0100

    Day 18 - b does not work

--- 2017/18/18b.py
+++ 2017/18/18b.py
@@ -25,8 +25,9 @@ def mod(variables, var, val):
     variables[var] = variables[var] % val
     return variables
 
-def rcv(sound):
-    print(sound)
+def rcv(variables, var, val):
+    variables[var] = val
+    return variables
 
 def jgz(variables, var, val):
     if variables[var] > 0:
@@ -41,45 +42,88 @@ with open('i', 'r') as f:
     inp = f.read().split('\n')
 
 
+variables_A = {'p': 0}
+variables_B = {'p': 1}
 
-variables = {}
-sound = 0
+A_queue = []
+B_queue = []
 i = 0
+
 while i < len(inp):
-    instruction = parse(variables, inp[i])
+    instruction = parse(variables_A, inp[i])
     if instruction[0] == 'jgz':
         try:
-            i += jgz(variables, instruction[1], instruction[2]) - 1
+            i += jgz(variables_A, instruction[1], instruction[2]) - 1
         except KeyError:
             pass
     elif instruction[0] == 'set':
         try:
-            variables = set(variables, instruction[1], instruction[2])
+            variables_A = set(variables_A, instruction[1], instruction[2])
         except KeyError:
             pass
     elif instruction[0] == 'mul':
         try:
-            variables = mul(variables, instruction[1], instruction[2])
+            variables_A = mul(variables_A, instruction[1], instruction[2])
         except KeyError:
             pass
     elif instruction[0] == 'add':
         try:
-            variables = add(variables, instruction[1], instruction[2])
+            variables_A = add(variables_A, instruction[1], instruction[2])
         except KeyError:
             pass            
     elif instruction[0] == 'mod':
         try:
-            variables = mod(variables, instruction[1], instruction[2])
+            variables_A = mod(variables_A, instruction[1], instruction[2])
         except KeyError:
             pass            
     elif instruction[0] == 'snd':
         try: 
-            sound = snd(variables, instruction[1])
+            A_queue.append(snd(variables_A, instruction[1]))
         except KeyError:
             pass            
     elif instruction[0] == 'rcv':
-        rcv(sound)
         break
     i += 1
-
-
+print(A_queue)
+i = 0
+while i < len(inp):
+    instruction = parse(variables_B, inp[i])
+    if instruction[0] == 'jgz':
+        try:
+            i += jgz(variables_B, instruction[1], instruction[2]) - 1
+        except KeyError:
+            pass
+    elif instruction[0] == 'set':
+        try:
+            variables_B = set(variables_B, instruction[1], instruction[2])
+        except KeyError:
+            pass
+    elif instruction[0] == 'mul':
+        try:
+            variables_B = mul(variables_B, instruction[1], instruction[2])
+        except KeyError:
+            pass
+    elif instruction[0] == 'add':
+        try:
+            variables_B = add(variables_B, instruction[1], instruction[2])
+        except KeyError:
+            pass            
+    elif instruction[0] == 'mod':
+        try:
+            variables_B = mod(variables_B, instruction[1], instruction[2])
+        except KeyError:
+            pass            
+    elif instruction[0] == 'snd':
+        try: 
+            B_queue.append(snd(variables_B, instruction[1]))
+        except KeyError:
+            pass            
+    elif instruction[0] == 'rcv':
+        try:
+            variables_B = rcv(variables_B, instruction[1], A_queue.pop(0))
+        except KeyError:
+            pass
+        except IndexError:
+            print(len(B_queue)) 
+            break        
+    i += 1
